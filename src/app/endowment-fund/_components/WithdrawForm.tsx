8aa8a425152ea2fcefe2c3639cc593defd5b1cf7
Bobby Fiando <bobfiando@gmail.com>
2024-11-20 08:50:42 +0700

    Integrate withdraw

--- src/app/endowment-fund/_components/WithdrawForm.tsx
+++ src/app/endowment-fund/_components/WithdrawForm.tsx
@@ -2,63 +2,177 @@ import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
 import Image from "next/image";
 import GetContractBalance from "./GetContractBalance";
-import { useAccount } from "wagmi";
+import {
+  useAccount,
+  useReadContract,
+  useWaitForTransactionReceipt,
+  useWriteContract,
+} from "wagmi";
 import { HexAddress } from "@/types/types";
+import { abi as EduenaAbi } from "@/abis/Eduena";
+import { abi as StakedUSDe } from "@/abis/StakedUSDe";
+import { BaseError, parseEther, formatEther } from "viem";
+import { useState, useEffect } from "react";
+import { debounce } from "lodash";
+import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/react";
 
 export default function WithdrawForm() {
   const account = useAccount();
-  async function submit(e: React.FormEvent<HTMLFormElement>) {
+  const [amount, setAmount] = useState("");
+  const [debouncedAmount, setDebouncedAmount] = useState(amount);
+
+  // Debounce the amount
+  useEffect(() => {
+    const handler = debounce(() => {
+      setDebouncedAmount(amount);
+    }, 300);
+
+    handler();
+
+    return () => {
+      handler.cancel();
+    };
+  }, [amount]);
+
+  const { data: receiveAmount } = useReadContract({
+    abi: StakedUSDe,
+    address: process.env.NEXT_PUBLIC_SUSDE_CONTRACT_ADDRESS as HexAddress,
+    functionName: "previewWithdraw",
+    args: [BigInt(parseEther(debouncedAmount.toString()))],
+  });
+
+  const {
+    isOpen: modalIsOpen,
+    onOpen: modalOnOpen,
+    onOpenChange: modalOnOpenChange,
+  } = useDisclosure();
+  const {
+    data: hash,
+    writeContract,
+    isPending,
+    error,
+    reset: resetWriteContract,
+  } = useWriteContract();
+  const { isLoading: isConfirming, isSuccess: isConfirmed } =
+    useWaitForTransactionReceipt({
+      hash,
+    });
+
+  function withdrawStakedUSDe(e: React.FormEvent<HTMLFormElement>): void {
     e.preventDefault();
+
+    resetWriteContract();
+
+    writeContract({
+      address: process.env.NEXT_PUBLIC_ENDOWMENT_FUND_ADDRESS as HexAddress,
+      abi: EduenaAbi,
+      functionName: "withdraw",
+      args: [BigInt(parseEther(amount.toString()))],
+    });
+
+    modalOnOpen();
   }
 
   return (
-    <form className="bg-white rounded-lg shadow-lg p-6" onSubmit={submit}>
-      <div className="mb-6">
-        <div className="flex items-center">
-          <div className="flex-grow">
-            <div className="flex items-center">
-              <Input
-                type="number"
-                label="Withdraw"
-                placeholder=""
-                id="amount"
-                name="amount"
-              />
-              <div className="flex w-48">
-                <Image
-                  src="img/sUSDe.svg"
-                  alt="sUSDe Logo"
-                  className="w-6 h-6 flex-shrink-0 ml-2"
-                  width={24}
-                  height={24}
+    <>
+      <form
+        className="bg-white rounded-lg shadow-lg p-6"
+        onSubmit={withdrawStakedUSDe}
+      >
+        <div className="mb-6">
+          <div className="flex items-center">
+            <div className="flex-grow">
+              <div className="flex items-center">
+                <Input
+                  type="number"
+                  label="Withdraw"
+                  placeholder=""
+                  name="amount"
+                  value={amount.toString()}
+                  onChange={(e) => setAmount(e.target.value)}
                 />
-                <span className="ml-2 flex-shrink-0">sUSDe</span>
+                <div className="flex w-48">
+                  <Image
+                    src="/img/endowment-fund.jpg"
+                    alt="Endowment Fund Logo"
+                    className="w-6 h-6 flex-shrink-0 ml-2"
+                    width={24}
+                    height={24}
+                  />
+                  <span className="ml-2 flex-shrink-0">EDN</span>
+                </div>
               </div>
             </div>
           </div>
+          {account.status === "connected" && (
+            <p className="text-gray-600 mt-2 text-right text-sm">
+              <span className="font-bold">
+                Balance:{" "}
+                <GetContractBalance
+                  address={account.address}
+                  contract={
+                    process.env
+                      .NEXT_PUBLIC_SUSDE_CONTRACT_ADDRESS! as HexAddress
+                  }
+                />
+              </span>
+            </p>
+          )}
         </div>
-        {account.status === "connected" && (
-          <p className="text-gray-600 mt-2 text-right text-sm">
+        <div className="mt-4 p-4 bg-gray-100 rounded-lg mb-4">
+          <p className="text-gray-600">
+            You receive:{" "}
             <span className="font-bold">
-              Balance:{" "}
-              <GetContractBalance
-                address={account.address}
-                contract={
-                  process.env.NEXT_PUBLIC_SUSDE_CONTRACT_ADDRESS! as HexAddress
-                }
-              />
+              {receiveAmount ? Math.floor(Number(formatEther(BigInt(receiveAmount)))).toString() + " " + "sUSDe" : ""}
             </span>
           </p>
-        )}
-      </div>
-      <div className="mt-4 p-4 bg-gray-100 rounded-lg mb-4">
-        <p className="text-gray-600">
-          You receive: <span className="font-bold">123</span>
-        </p>
-      </div>
-      <Button color="primary" type="submit" className="w-full block mt-4">
-        Withdraw
-      </Button>
-    </form>
+        </div>
+        <Button color="primary" type="submit" className="w-full block mt-4">
+          Withdraw
+        </Button>
+      </form>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onOpenChange={modalOnOpenChange}
+        isDismissable={false}
+        isKeyboardDismissDisabled={true}
+        hideCloseButton={true}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Deposit</ModalHeader>
+              <ModalBody>
+                {hash && <div>Transaction Hash: {hash}</div>}
+                {isPending ? "Withdrawing..." : "Deposit"}
+                {isConfirming && <div>Waiting for confirmation...</div>}
+                {isConfirmed && (
+                  <>
+                    <div>Transaction confirmed.</div>
+                  </>
+                )}
+                {error && (
+                  <div
+                    className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                    role="alert"
+                  >
+                    <strong className="font-bold">Error: </strong>
+                    <span className="block sm:inline">
+                      {(error as BaseError).shortMessage || error.message}
+                    </span>
+                  </div>
+                )}
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
