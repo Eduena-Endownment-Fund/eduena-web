a6c76584dd1ef3259a75f2e970f4f549e67d8ce4
Bobby Fiando <bobfiando@gmail.com>
2024-11-21 19:27:28 +0700

    Update information section

--- src/app/endowment-fund/_components/InformationSection.tsx
+++ src/app/endowment-fund/_components/InformationSection.tsx
@@ -3,10 +3,7 @@ const InformationSection = () => {
     <div className="bg-white rounded-lg shadow-lg p-6 mt-6">
       <h2 className="text-xl font-bold text-gray-800 mb-4">Information</h2>
       <p className="text-gray-600">
-        The deposited funds are used to support various initiatives and projects
-        for verified beneficiaries. All profits generated from the deposits are
-        utilized to fund these initiatives, ensuring transparency and security
-        in the process.
+      The deposited funds are exclusively used to generate yields, ensuring that the main fund remains secure. All profits generated from these deposits are utilized to support various initiatives and projects for verified beneficiaries, maintaining transparency and security throughout the process.
       </p>
     </div>
   );
